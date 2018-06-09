6f68cc37977cd11da8f2915ebec15b2bc1536c27
dpendergast <dlp75@case.edu>
2018-06-09 17:53:27 -0400

    initial commit. opengl is up and running

---
+++ src/items/item.py
@@ -0,0 +1,30 @@
+from enum import Enum
+
+class StatType(Enum):
+    ATT = 0,
+    DEF = 1,
+    VIT = 2,
+    LVL = 3,
+    ATTACK_RADIUS = 4,
+    MOV_SPD_WHILE_ATTACKING = 5,
+    ATTACK_SPEED = 6,
+    ATTACK_DAMAGE= 7,
+    MOV_SPD_OF_ENEMIES_IN_RADIUS = 8,
+    MOVEMENT_SPEED = 9,
+    DODGE = 10,
+    ACCURACY = 11,
+    LIFE_REGEN = 12,
+    LIFE_ON_HIT = 13,
+    LIFE_LEECH = 14,
+    MAX_HEALTH = 15,
+    POTION_HEALING = 16,
+    POTION_COOLDOWN = 17
+    
+    
+    HOLE_BONUS = 18
+    
+def getStatDescriptor
+
+
+if __name__ == "__main__":
+     print(StatType.ATT)
